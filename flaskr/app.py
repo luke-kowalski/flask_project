eb1ee7a291d3b0da3c91ca00cad7ed2f67b1befe
LK <lukasz.fialki@gmail.com>
2022-07-15 15:21:52 +0200

    index render template added

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,11 @@
 from unicodedata import name
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "elo mordo"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
